3e8de5fede4d0956f97a82a73bcb675ec9ca3d16
David Grankvist <grankvist.david@gmail.com>
2021-08-29 00:51:54 +0200

    Add comments

--- js/index.js
+++ js/index.js
@@ -1,18 +1,21 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.132.2";
+
+// init
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// create cube
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-
+// zoom out to see cube
 camera.position.z = 5;
 
+// render loop
 const animate = () => {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
