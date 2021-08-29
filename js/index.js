dbefcf4e39bce06ccdf4253d996b44d96bfaa598
David Grankvist <grankvist.david@gmail.com>
2021-08-29 10:18:42 +0200

    Render 3D grid of cubes with instancing

--- js/index.js
+++ js/index.js
@@ -12,19 +12,33 @@ const spotLight = new THREE.SpotLight(0xffffff);
 spotLight.position.set(200, 400, 300);
 scene.add(spotLight);
 
-// create cube
+// create 3D grid of cubes
+const dim = 100;
+const numCubes = dim * dim * dim;
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshLambertMaterial({ color: 0x00bbaa , emissive: 0x0000aa });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-// zoom out to see cube
-camera.position.z = 5;
+const cubes = new THREE.InstancedMesh(geometry, material, numCubes);
+const matrix = new THREE.Matrix4();
+
+let i = 0;
+for (let x = 0; x < dim; x++) {
+    for (let y = 0; y < dim; y++) {
+        for (let z = 0; z < dim; z++) {
+            matrix.setPosition(x, y, z);
+            cubes.setMatrixAt(i, matrix);
+            i++;
+        }
+    }
+}
+scene.add(cubes);
+
+// zoom out to see graphics
+camera.position.set(dim * 2, dim * 2, dim * 2);
+camera.lookAt(0, 0, 0);
 
 // render loop
 const animate = () => {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
 };
 animate();
