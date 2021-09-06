969c32edadbca2a26c46a02f99eee2b804b74e5f
David Grankvist <grankvist.david@gmail.com>
2021-09-06 18:35:00 +0200

    Change CA graphics instance count based on state

--- js/ca-canvas/scene/CaGraphics.js
+++ js/ca-canvas/scene/CaGraphics.js
@@ -12,18 +12,19 @@ export default class CaGraphics {
     #matrixHide;
     #matrixShow;
 
+    #maxDims;
     #dims;
-    #dimsHalf; // center grid around origin
 
-    constructor(dims) {
-        this.#dims = dims;
+    constructor(maxDims) {
+        this.#maxDims = maxDims;
+        this.#dims = maxDims;
 
         // cell graphics
         const geometry = new THREE.BoxGeometry();
         const material = new THREE.MeshLambertMaterial({ color: 0x00bbaa, emissive: 0x0000aa});
 
         // instancing
-        const numCubes = dims.x * dims.y * dims.z;
+        const numCubes = this.#maxDims.x * this.#maxDims.y * this.#maxDims.z;
         this.#mesh = new THREE.InstancedMesh(geometry, material, numCubes);
 
         // cell transforms
@@ -35,12 +36,6 @@ export default class CaGraphics {
             0, 0, 0, 0,
             0, 0, 0, 0
         );
-
-        this.#dimsHalf = {
-            x: Math.round(dims.x / 2),
-            y: Math.round(dims.y / 2),
-            z: Math.round(dims.z / 2)
-        };
     }
 
     getMesh() {
@@ -54,9 +49,9 @@ export default class CaGraphics {
                 break;
             case BinaryCell.ON:
                 this.#matrixShow.setPosition(
-                    x - this.#dimsHalf.x,
-                    y - this.#dimsHalf.y,
-                    z - this.#dimsHalf.z
+                    x - Math.round(this.#dims.x / 2),
+                    y - Math.round(this.#dims.y / 2),
+                    z - Math.round(this.#dims.z / 2)
                 );
                 this.#mesh.setMatrixAt(i, this.#matrixShow);
                 break;
@@ -67,10 +62,14 @@ export default class CaGraphics {
     // subscription
     update(caState) {
         const dims = caState.getDimensions();
-        if (dims.x !== this.#dims.x ||
+        if (dims.x > this.#maxDims.x ||
+            dims.y > this.#maxDims.y ||
+            dims.z > this.#maxDims.z) {
+            throw new Error("CA state dimensions out of bounds.");
+        } else if (dims.x !== this.#dims.x ||
             dims.y !== this.#dims.y ||
             dims.z !== this.#dims.z) {
-            throw new Error("Called update with mismatching state dimensions.");
+            this.#dims = dims;
         }
 
         let i = 0;
@@ -84,5 +83,6 @@ export default class CaGraphics {
             }
         }
         this.#mesh.instanceMatrix.needsUpdate = true;
+        this.#mesh.count = i;
     }
 }
