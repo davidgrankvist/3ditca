05e274e257c2bad61dccd1f2cc7a2fab147e7f41
David Grankvist <grankvist.david@gmail.com>
2021-09-11 22:52:44 +0200

    Register components in separate script

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,1 @@
-import "./ca-header.js";
-import "./ca-canvas/ca-canvas.js";
-import "./ca-param-form.js";
-import "./ca-tools.js";
+import "./register-all.js";
---
+++ src/js/register-all.js
@@ -0,0 +1,4 @@
+import "./ca-header.js";
+import "./ca-canvas/ca-canvas.js";
+import "./ca-param-form.js";
+import "./ca-tools.js";
