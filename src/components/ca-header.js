fb5cfe2f26233d02a7c54ad7203208068d6012a7
David Grankvist <grankvist.david@gmail.com>
2021-09-12 14:33:53 +0200

    Add FA icons in header
    - add font awesome dependency
    - use icons for tools and play/pause

--- src/components/ca-header.js
+++ src/components/ca-header.js
@@ -1,14 +1,29 @@
 class CaHeader extends HTMLElement {
+    #state;
+
     constructor() {
         super();
+        this.#state = { play: true };
     }
 
     connectedCallback() {
         this.innerHTML = `
             <nav class="navbar" role="navigation" aria-label="main navigation">
                 <div class="navbar-brand">
-                    <a id="nav-tools" class="navbar-item">Tools</a>
-                    <a id="nav-play" class="navbar-item">Play</a>
+                    <div class="navbar-item">
+                        <a id="nav-tools" class="button>
+                            <span class="icon">
+                                <i class="fas fa-cog"></i>
+                            </span>
+                        </a>
+                    </div>
+                    <div class="navbar-item">
+                        <a id="nav-play-btn" class="button>
+                            <span class="icon">
+                                <i id="nav-play-icon" class="fas fa-play"></i>
+                            </span>
+                        </a>
+                    </div>
                 </div>
             </nav>
         `;
@@ -22,16 +37,17 @@ class CaHeader extends HTMLElement {
             tools.setAttribute("visible", visible === "true" ? "false" : "true");
         });
 
-        const playBtn = document.getElementById("nav-play");
-        playBtn.addEventListener("click", () => {
+        const playBtnIcon = document.getElementById("nav-play-icon");
+        document.getElementById("nav-play-btn").addEventListener("click", () => {
             const canvas = document.querySelector("ca-canvas");
-            if (playBtn.innerText === "Play") {
-                playBtn.innerText = "Pause";
+            if (this.#state.play) {
+                playBtnIcon.classList.replace("fa-play", "fa-pause");
                 canvas.dispatchEvent(new CustomEvent("ca-play"));
             } else {
-                playBtn.innerText = "Play ";
+                playBtnIcon.classList.replace("fa-pause", "fa-play");
                 canvas.dispatchEvent(new CustomEvent("ca-pause"));
             }
+            this.#state.play = !this.#state.play;
         });
     }
 }
